76da4057c37da1c115b71d7de2a34f235963dd0a
Vedanta SP <vedant.vasu1111@gmail.com>
2023-03-21 17:15:27 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,18 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
+
 }
 class _LoginScreenState extends State<LoginScreen> {
   bool _isLoading = false;
+
+  void _handleSkipSignIn() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  }
+
 
   Future<void> _handleSignIn() async {
     try {
@@ -23,35 +32,42 @@ class _LoginScreenState extends State<LoginScreen> {
       });
 
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+      final GoogleSignInAuthentication googleAuth = await googleUser!
+          .authentication;
 
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      final UserCredential userCredential = await _auth.signInWithCredential(
+          credential);
       final User? user = userCredential.user;
 
       if (user != null) {
         // User signed in successfully
-        Navigator.of(context).pushReplacementNamed('/home');
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
       } else {
         // User could not be signed in
         showDialog(
           context: context,
-          builder: (context) => AlertDialog(
-            title: Text('Sign-In Error'),
-            content: Text('Unable to sign in to the app.'),
-            actions: <Widget>[
-              TextButton(
-                child:  Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+          builder: (context) =>
+              AlertDialog(
+                title: Text('Sign-In Error'),
+                content: Text('Unable to sign in to the app.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
         );
       }
     } catch (e) {
@@ -61,18 +77,19 @@ class _LoginScreenState extends State<LoginScreen> {
       });
       showDialog(
         context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Sign-In Error'),
-          content: Text(e.toString()),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('OK'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+        builder: (context) =>
+            AlertDialog(
+              title: const Text('Sign-In Error'),
+              content: Text(e.toString()),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
       );
     }
   }
@@ -80,15 +97,43 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Login Screen'),
+        toolbarOpacity: 0.5,
+        toolbarTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        title: const Text('Login / Sign Up'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
       ),
       body: Center(
         child: _isLoading
-            ? CircularProgressIndicator()
-            : ElevatedButton(
-          onPressed: _handleSignIn,
-          child: const Text('Sign in with Google'),
+            ? const CircularProgressIndicator()
+            : Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(child: Container()),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                minimumSize: const Size(200, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+              onPressed: _handleSignIn,
+              child: const Text('Sign in with Google'),
+            ),
+            const SizedBox(height: 20.0),
+            TextButton(
+              onPressed: _handleSkipSignIn,
+              child: const Text('Skip Sign In'),
+            ),
+          ],
         ),
       ),
     );
@@ -109,11 +154,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Milan App',
+      title: 'Milan',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      home:  SplashScreen(),
     );
   }
 }
@@ -148,7 +193,7 @@ class _SplashScreenState extends State<SplashScreen>
       const Duration(seconds: 3),
       () => Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
+        MaterialPageRoute(builder: (context) => LoginScreen()),
       ),
     );
   }
@@ -203,7 +248,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.grey[800],
+      backgroundColor: Colors.grey[900],
       body: Stack(
         children: [
           const Positioned(
@@ -311,6 +356,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           }).then((value) {
                             // Success
                             print('Data saved successfully');
+                            Navigator.pop(context);
                           }).catchError((error) {
                             // Error
                             print('Failed to save data: $error');
@@ -332,72 +378,123 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
+
+@override
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  late Stream<QuerySnapshot> _stream;
+  final TextEditingController _searchController = TextEditingController();
+
+  TextEditingController get searchController => _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    _stream = FirebaseFirestore.instance.collection('supplies').snapshots();
+  }
+  Future<List<DocumentSnapshot>> getSuppliesData(String address) async {
+    print('Location: $address');
+    final firestore = FirebaseFirestore.instance;
+    final querySnapshot =
+    await firestore.collection('supplies').where('address', isEqualTo: address).get();
+    print('Query snapshot length: ${querySnapshot.docs.length}');
+    querySnapshot.docs.forEach((doc) {
+      print(doc.data());
+    });
+    return querySnapshot.docs;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-            top: 80.0,
-            left: 16.0,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [Colors.grey[900]!, Colors.grey[800]!],
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey[900]!,
-                    blurRadius: 4.0,
-                    offset: const Offset(2.0, 2.0),
+          SizedBox(height: MediaQuery.of(context).padding.top), // Space for status bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Search by location...',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    ),
                   ),
-                ],
-              ),
-              padding: EdgeInsets.all(16.0),
-              child: const Text(
-                'Supplies Needed',
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Lato',
-                  color: Colors.white,
                 ),
-              ),
+                SizedBox(width: 16.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    final suppliesData = await getSuppliesData(_searchController.text);
+                    // Use the suppliesData list to display your UI based on the search query
+                    // ...
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
             ),
           ),
-          Center(
-            child: Text(
-              'Form Data',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Lato',
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 40.0,
-            left: 16.0,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
+          Expanded(
+            child: FutureBuilder<List<DocumentSnapshot>>(
+              future: getSuppliesData(_searchController.text),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                final suppliesData = snapshot.data ?? [];
+                final columns = ['Name', 'Address', 'Supplies'];
+                final rows = suppliesData!
+                    .map((doc) => DataRow(cells: [
+                  DataCell(Text(doc['name'] ?? 'N/A')),
+                  DataCell(Text(doc['address'] ?? 'N/A')),
+                  DataCell(Text(doc['supplies'] ?? 'N/A')),
+                ]))
+                    .toList();
+
+                return DataTable(
+                  columns: columns.map((col) => DataColumn(label: Text(col))).toList(),
+                  rows: rows,
+                  columnSpacing: 16.0,
+                  dataRowHeight: 72.0,
+                  headingRowHeight: 96.0,
+                  dividerThickness: 0.5,
+                  horizontalMargin: 16.0,
+                  dataTextStyle: const TextStyle(color: Colors.white, fontSize: 20.0),
+                  headingTextStyle: const TextStyle(color: Colors.white, fontSize: 20.0),
+
+                );
               },
-              child: Container(
-                color: Colors.transparent,
-                width: 50,
-                child: const Icon(Icons.arrow_back_ios_new_sharp),
-              ),
             ),
+          ),
+        //add a back button to homescreen
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Back'),
           ),
         ],
       ),
     );
   }
+
 }
